2a027302c20ffdfb4df479f84fc3335a2af4d839
Craig Siemens <siemens.craig@gmail.com>
2013-09-05 08:56:16 -0600

    Added second version of the test that works with Kiwi 2.2.1

--- KiwiTestTests/TestSpec.m
+++ KiwiTestTests/TestSpec.m
@@ -13,6 +13,11 @@ SPEC_BEGIN(TestSpec)
 describe(@"Test", ^{
 
     it(@"should work", ^{
+        [[UIView should] receive:@selector(animateWithDuration:animations:) withArguments:theValue(0.25f), any()];
+        [UIView animateWithDuration:0.25f animations:nil];
+    });
+    
+    it(@"should work", ^{
         [[[UIView should] receive] animateWithDuration:0.25f animations:any()];
         [UIView animateWithDuration:0.25f animations:nil];
     });
